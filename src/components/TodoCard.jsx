8d666c37422fbc01f079899ed0d916b988e1b6d2
junaeidysetiawan <junaeidys0@gmail.com>
2025-03-17 16:17:16 +0700

    Using external styling techniques(Tailwind css and HeroUI)

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -1,13 +1,33 @@
+import { Button, Card, CardBody, CardFooter, CardHeader, Divider } from "@heroui/react";
+
 const TodoCard = () => {
+  // return (
+  //   <div>
+  //     <h4>Monday Card</h4>
+  //     <ul>
+  //       <li>Coding</li>
+  //       <li>Mandi </li>
+  //       <li>Makan</li>
+  //     </ul>
+  //   </div>
+  // )
+
   return (
-    <div>
-      <h4>Monday Card</h4>
-      <ul>
-        <li>Coding</li>
-        <li>Mandi </li>
-        <li>Makan</li>
-      </ul>
-    </div>
+    <Card className="max-w-[400px]">
+      <CardHeader className="font-bold text-lg">Monday</CardHeader>
+      <Divider/>
+      <CardBody>
+        <ul className="list-decimal list-inside">
+          <li>Coding</li>
+          <li>Mandi </li>
+          <li>Makan</li>
+        </ul>
+      </CardBody>
+      <Divider/>
+      <CardFooter>
+        <Button>Add</Button>
+      </CardFooter>
+    </Card>
   )
 }
 
